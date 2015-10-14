9a95c08664a688f24601d1938a02f1c57c1bc860
Daniela Simões <danielasimoes@ua.pt>
2015-10-14 17:37:34 +0100

    Update e1a.s

--- Aula 4/e1a.s
+++ Aula 4/e1a.s
@@ -1,36 +1,36 @@
-	# Rafael Ferreira https://github.com/gipmon/ac1
-
+# exercício com índices	
+	
 	.data
-str: 	.space 20
-	.eqv SIZE, 4
-	.text
-	.globl main
-main:
-	la $a0, str #onde vai colocar a string > buffer
-	li $a1, 20  #a string lida vai ter tamanho 20 
-	li $v0, 8
+str:	.space 20
+ 
+ 	.text
+ 	.globl main
+ 	
+main: 
+	la $a0, str #onde a str vai ser armazenada
+	li $v0, 8 #opção read_string
+	ori $a1, $0, 20 #tamanho da str
 	syscall
-	#
-	li $t0, 0 #num = 0
-	li $t1, 0 #i = 0
-	#
-	#la $t2, str #colocar em $t2 a str
+	
+	# num = 0
+	li $t0, 0
+	# i = 0
+	li $t1, 0
+
 while:
-	#addu $t4, $t2, $t1
-	#$t4 = $t2 + $t1 ;
-	#faz o load byte para $t3 do $t4 no offset 0 
-	#lb $t3, 0($t4) 
-	#faz o load byte para $t3 diretamente da str com offset $t1
 	lb $t3, str($t1)
-	#condicao do while
-	beq $t3, '\0', endwhile
-	#se estiver entre 0 e 9: num++
-	blt $t3, '0', endif
-	bgt $t3, '9', endif
+	#  str[i] == '\n'
+	beq $t3, '\n', endwhile
+	
+	# num++
 	addi $t0, $t0, 1
+	bltu $t3, '0', endif
+	bgtu $t3, '9', endif
+
 endif:
-	addiu $t1, $t1, 1 # i++
+	addi $t1, $t1, 1
 	j while
+	
 endwhile:
 	li $v0, 1
 	or $a0, $0, $t0
